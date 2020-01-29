22c5e1550339fe7bfa2540528ce38f717cf8e943
raythurman2386 <raymondthurman5@gmail.com>
2020-01-29 12:35:28 -0500

    parent and child tables

--- data/migrations/20200128220906_initial.js
+++ data/migrations/20200128220906_initial.js
@@ -1,8 +1,30 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('parents', tbl => {
+    tbl.increments('id');
+    tbl.string('username', 128).unique().notNullable();
+    tbl.string('password', 128).notNullable();
+    tbl.string('email', 128).unique().notNullable()
+  });
+  await knex.schema.createTable('child', tbl => {
+    tbl.increments('id');
+    tbl.string('name', 128).notNullable();
+    tbl.integer('parent_id')
+      .unsigned()
+      .references('id')
+      .inTable('parents')
+      .onDelete('CASCADE')
+      .onUpdate("CASCADE");
+    tbl.integer('co_parent_id')
+       .unsigned()
+       .references('id')
+       .inTable('parents')
+       .onDelete('CASCADE')
+       .onUpdate("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('child');
+  await knex.schema.dropTableIfExists('parents');
 };
