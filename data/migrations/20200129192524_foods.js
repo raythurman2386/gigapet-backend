52eaa1de4e831f3e51c0361c28cc6334b09ec751
raythurman2386 <raymondthurman5@gmail.com>
2020-01-29 19:58:47 -0500

    tables defined

---
+++ data/migrations/20200129192524_foods.js
@@ -0,0 +1,19 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("foods", tbl => {
+    tbl.increments("id");
+    tbl.timestamps(true, true);
+    tbl
+      .integer("child_id")
+      .notNullable()
+      .references("id")
+      .inTable("child")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl.string("type").notNullable();
+    tbl.integer("servings").notNullable();
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("foods");
+};
