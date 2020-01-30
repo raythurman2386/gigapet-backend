b869799d5e006a6d575c034d772c8ab84329a12e
raythurman2386 <raymondthurman5@gmail.com>
2020-01-30 11:00:41 -0500

    fixed login routes

--- data/seeds/01-parents.js
+++ data/seeds/01-parents.js
@@ -1,34 +1,38 @@
+const bcrypt = require('bcryptjs')
+
 exports.seed = async function(knex) {
-  await knex("parents").insert([
+  const pw = await bcrypt.hash('test', 12)
+
+  await knex('parents').insert([
     {
-      parent_name: "test1",
-      username: "test1",
-      password: "test",
-      email: "test1@test.com"
+      parent_name: 'test1',
+      username: 'test1',
+      password: pw,
+      email: 'test1@test.com'
     },
     {
-      parent_name: "test2",
-      username: "test2",
-      password: "test",
-      email: "test2@test.com"
+      parent_name: 'test2',
+      username: 'test2',
+      password: pw,
+      email: 'test2@test.com'
     },
     {
-      parent_name: "test3",
-      username: "test3",
-      password: "test",
-      email: "test3@test.com"
+      parent_name: 'test3',
+      username: 'test3',
+      password: pw,
+      email: 'test3@test.com'
     },
     {
-      parent_name: "test4",
-      username: "test4",
-      password: "test",
-      email: "test4@test.com"
+      parent_name: 'test4',
+      username: 'test4',
+      password: pw,
+      email: 'test4@test.com'
     },
     {
-      parent_name: "test5",
-      username: "test5",
-      password: "test",
-      email: "test5@test.com"
+      parent_name: 'test5',
+      username: 'test5',
+      password: pw,
+      email: 'test5@test.com'
     }
-  ]);
-};
+  ])
+}
