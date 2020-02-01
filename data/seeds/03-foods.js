bbc7093b1c742cff0f191e51ca39f748f84cf0df
raythurman2386 <raymondthurman5@gmail.com>
2020-01-31 23:43:50 -0500

    refactoring migrations

--- data/seeds/03-foods.js
+++
@@ -1,9 +0,0 @@
-exports.seed = async function(knex) {
-  await knex('foods').insert([
-    { name: 'food', child_id: 1, type: 'fruits', servings: '2' },
-    { name: 'food', child_id: 1, type: 'veggies', servings: '3' },
-    { name: 'food', child_id: 3, type: 'snack', servings: '2' },
-    { name: 'food', child_id: 4, type: 'meat', servings: '1' },
-    { name: 'food', child_id: 5, type: 'snack', servings: '1' }
-  ])
-}
--- data/seeds/04-monsters.js
+++
@@ -1,8 +0,0 @@
-exports.seed = async function(knex) {
-  await knex('monsters').insert([
-    { name: 'monster-orange' },
-    { name: 'monster-red' },
-    { name: 'monster-green' },
-    { name: 'monster-purple' }
-  ])
-}
