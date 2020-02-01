c50bb32d98812c0497c87cd52e8f60c13a7e4ce2
raythurman2386 <raymondthurman5@gmail.com>
2020-02-01 10:09:33 -0500

    fixed seed files

--- data/migrations/20200131234036_children.js
+++ data/migrations/20200131234036_children.js
@@ -2,7 +2,13 @@ exports.up = async function(knex) {
   await knex.schema.createTable('child', tbl => {
     tbl.increments('id')
     tbl.string('name', 128).notNullable()
-    tbl.integer('monster_id').notNullable()
+    tbl
+      .integer('monster_id')
+      .references('id')
+      .inTable('monsters')
+      .notNullable()
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
     tbl
       .integer('parent_id')
       .unsigned()
