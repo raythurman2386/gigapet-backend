ad70d555c794525841d1a896089c4cf9a8d94b6b
raythurman2386 <raymondthurman5@gmail.com>
2020-02-03 11:43:55 -0500

    update working correctly

--- router/children/child.spec.js
+++ router/children/child.spec.js
@@ -1,5 +1,10 @@
 const supertest = require('supertest')
 const server = require('../../api/server')
+const db = require('../../data/db-config')
+
+beforeEach(async () => {
+  await db.seed.run()
+})
 
 describe('child routes', () => {
   test('should get children', async () => {
@@ -48,17 +53,22 @@ describe('child routes', () => {
     expect(res.body[0]).toBe(6)
   })
 
-  // test('should update a child', async () => {
-  //   const updated = { name: 'Bobina', monster_id: '1', parent_id: '1' }
+  test('should update a child', async () => {
+    const updated = { name: 'Bobina', monster_id: '1', parent_id: '1' }
 
-  //   const res = await supertest(server)
-  //     .put('/api/child/1')
-  //     .send(updated)
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
 
-  //   expect(res.status).toBe(200)
-  //   expect(res.type).toBe('application/json')
-  //   expect(res.body.message).toContain('Updated')
-  // })
+    const res = await supertest(server)
+      .put('/api/child/1')
+      .send(updated)
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(200)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toContain('Updated')
+  })
 
   // test('should delete a child', async () => {
   //   const res = await supertest(server).delete('/api/child/1')
