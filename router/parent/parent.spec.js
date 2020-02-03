2f9285f8e5af5c3cec99da68d8653186e0947405
raythurman2386 <raymondthurman5@gmail.com>
2020-02-03 16:26:41 -0500

    get parent by id test working

--- router/parent/parent.spec.js
+++ router/parent/parent.spec.js
@@ -1,5 +1,23 @@
-describe('parent routes', () => {
-  test('should add 2+2', () => {
-    expect(2 + 2).toBe(4)
+const supertest = require('supertest')
+const server = require('../../api/server')
+const db = require('../../data/db-config')
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+describe('Parent Route Tests', () => {
+  test('should get parent by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .get('/api/parent/1')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(200)
+    expect(res.type).toBe('application/json')
+    expect(res.body.id).toBe(1)
   })
 })
