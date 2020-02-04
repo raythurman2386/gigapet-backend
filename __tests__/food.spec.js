63d1bfb77b7fefe38e33188ef258ab5a807e1a77
raythurman2386 <raymondthurman5@gmail.com>
2020-02-03 19:07:12 -0500

    food routes tested

--- __tests__/food.spec.js
+++ __tests__/food.spec.js
@@ -1,5 +1,76 @@
+const supertest = require('supertest')
+const server = require('../api/server')
+const db = require('../data/db-config')
+
+beforeEach(async () => {
+  return await db.seed.run()
+})
+
 describe('food routes', () => {
-  test('should add 2+2', () => {
-    expect(2 + 2).toBe(4)
+  test('should get food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .get('/api/foods/1')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(200)
+    expect(res.type).toBe('application/json')
+    expect(res.body.id).toBe(1)
+  })
+
+  test('should add a food', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .post('/api/foods')
+      .send({
+        name: 'new',
+        child_id: 1,
+        type: 'fruit',
+        servings: 5
+      })
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(201)
+    expect(res.type).toBe('application/json')
+    expect(res.body[0]).toBe(6)
+  })
+
+  test('should update a food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .put('/api/foods/1')
+      .send({
+        name: 'update',
+        child_id: 1,
+        type: 'fruit',
+        servings: 5
+      })
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(201)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/food updated/i)
+  })
+
+  test('should delete a food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .delete('/api/foods/1')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(200)
+    expect(res.type).toBe('application/json')
   })
 })
