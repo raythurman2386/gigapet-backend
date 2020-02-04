7d16abb3f3053be8dfe246091b006667f4521b7a
raythurman2386 <raymondthurman5@gmail.com>
2020-02-03 20:57:54 -0500

    food tests passing

--- __tests__/food.spec.js
+++ __tests__/food.spec.js
@@ -3,11 +3,11 @@ const server = require('../api/server')
 const db = require('../data/db-config')
 
 beforeEach(async () => {
-  return await db.seed.run()
+  await db.seed.run()
 })
 
 describe('food routes', () => {
-  test('should get food by id', async () => {
+  it('should get food by id', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -21,7 +21,21 @@ describe('food routes', () => {
     expect(res.body.id).toBe(1)
   })
 
-  test('should add a food', async () => {
+  it('should fail to get food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .get('/api/foods/111')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(404)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/There is no food by that ID/i)
+  })
+
+  it('should add a food', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -41,7 +55,26 @@ describe('food routes', () => {
     expect(res.body[0]).toBe(6)
   })
 
-  test('should update a food by id', async () => {
+  it('should fail to add a food', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .post('/api/foods')
+      .send({
+        name: 'new',
+        child_id: 1,
+        type: 'fruit'
+      })
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(400)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/Please send all required fields/i)
+  })
+
+  it('should update a food by id', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -61,7 +94,30 @@ describe('food routes', () => {
     expect(res.body.message).toMatch(/food updated/i)
   })
 
-  test('should delete a food by id', async () => {
+  it('should fail to update a food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'test1',
+        password: 'test'
+      })
+
+    const res = await supertest(server)
+      .put('/api/foods/111')
+      .send({
+        name: 'update',
+        child_id: 1,
+        type: 'fruit',
+        servings: 5
+      })
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(404)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/There is no food by that ID/i)
+  })
+
+  it('should delete a food by id', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -72,5 +128,20 @@ describe('food routes', () => {
 
     expect(res.status).toBe(200)
     expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/food deleted/i)
+  })
+
+  it('should fail to delete a food by id', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .delete('/api/foods/111')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(404)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/There is no food by that ID/i)
   })
 })
