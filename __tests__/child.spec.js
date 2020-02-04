24a4df6601dd814d8615290bcc6d0e7f03109975
raythurman2386 <raymondthurman5@gmail.com>
2020-02-03 20:57:47 -0500

    child spec passing

--- __tests__/child.spec.js
+++ __tests__/child.spec.js
@@ -7,7 +7,7 @@ beforeEach(async () => {
 })
 
 describe('child routes', () => {
-  test('should get children', async () => {
+  it('should get children', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -20,7 +20,7 @@ describe('child routes', () => {
     expect(res.type).toBe('application/json')
   })
 
-  test('should get child with id 1', async () => {
+  it('should get child with id 1', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -34,7 +34,21 @@ describe('child routes', () => {
     expect(res.body.child.id).toBe(1)
   })
 
-  test('should add child to db', async () => {
+  it('should fail getting child 111', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .get('/api/child/111')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(400)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/there is no child by that id/i)
+  })
+
+  it('should add child to db', async () => {
     const newChild = { name: 'test', monster_id: '1', parent_id: '1' }
 
     const login = await supertest(server)
@@ -51,7 +65,24 @@ describe('child routes', () => {
     expect(res.body[0]).toBe(6)
   })
 
-  test('should update a child', async () => {
+  it('should fail to add child to db', async () => {
+    const newChild = { name: 'test' }
+
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .post('/api/child')
+      .send(newChild)
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(400)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/Provide a name and a monster id please/i)
+  })
+
+  it('should update a child', async () => {
     const updated = { name: 'Bobina', monster_id: '1', parent_id: '1' }
 
     const login = await supertest(server)
@@ -68,7 +99,24 @@ describe('child routes', () => {
     expect(res.body.message).toContain('Updated')
   })
 
-  test('should delete a child', async () => {
+  it('should fail to update a child', async () => {
+    const updated = { name: 'Bobina' }
+
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .put('/api/child/111')
+      .send(updated)
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(400)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/there is no child by that id/i)
+  })
+
+  it('should delete a child', async () => {
     const login = await supertest(server)
       .post('/api/auth/login')
       .send({ username: 'test1', password: 'test' })
@@ -80,5 +128,19 @@ describe('child routes', () => {
     expect(res.status).toBe(200)
     expect(res.type).toBe('application/json')
     expect(res.body).toBe(1)
+  })
+
+  it('should fail to delete a child', async () => {
+    const login = await supertest(server)
+      .post('/api/auth/login')
+      .send({ username: 'test1', password: 'test' })
+
+    const res = await supertest(server)
+      .delete('/api/child/111')
+      .set('authorization', login.body.token)
+
+    expect(res.status).toBe(400)
+    expect(res.type).toBe('application/json')
+    expect(res.body.message).toMatch(/there is no child by that id/i)
   })
 })
