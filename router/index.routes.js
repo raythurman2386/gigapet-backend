cecef7c7916e3cb3e9595402828a6eb75eb85c00
raythurman2386 <raymondthurman5@gmail.com>
2020-02-21 19:46:10 -0500

    added graphql route

--- router/index.routes.js
+++ router/index.routes.js
@@ -3,9 +3,11 @@ const parentRouter = require('./parent/parent-router')
 const childRouter = require('./children/child-router')
 const foodRouter = require('./foods/food-router')
 const restricted = require('../middleware/restricted')
+const graphqlController = require('../controller/graphqlController')
 
 module.exports = server => {
   server.use('/api/auth', authRouter)
+  server.use('/graphql', graphqlController)
   server.use('/api/parent', restricted(), parentRouter)
   server.use('/api/child', restricted(), childRouter)
   server.use('/api/foods', restricted(), foodRouter)
