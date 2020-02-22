898b5c0477abdb67350ab3a051fe0f79670c794b
raythurman2386 <raymondthurman5@gmail.com>
2020-02-21 19:46:29 -0500

    graphql schema

---
+++ controller/graphqlController.js
@@ -0,0 +1,33 @@
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const { Food } = require('../models/Model')
+
+// Construct a schema
+const schema = buildSchema(`
+  type Query {
+    food(id: Int!): Food,
+    foods: [Food],
+  }
+  type Mutation {
+    createFood(name: String!, child_id: Int!, type: String!, servings: Int!): Food,
+    deleteFood(id: Int!): DeleteFoodResponse
+  }
+  input FoodInput {
+    name: String!,
+    child_id: Int,
+    type: String!,
+    servings: String!
+  }
+  type DeleteFoodResponse {
+    id: Int!
+  }
+  type Food {
+    id: Int!,
+    name: String!,
+    created_at: String,
+    child_id: Int,
+    type: String!,
+    servings: Int!
+  }
+`)
+
